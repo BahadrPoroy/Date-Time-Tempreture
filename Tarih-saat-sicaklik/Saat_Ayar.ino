c8ed6497de3f548162418103457db701d608adcc
BahadrPoroy <56046468+BahadrPoroy@users.noreply.github.com>
2024-10-25 22:27:05 +0300

    V2.0.2
    +Saat_Ayar.ino is updated and optimized.

--- Tarih-saat-sicaklik/Saat_Ayar.ino
+++ Tarih-saat-sicaklik/Saat_Ayar.ino
@@ -1,198 +1,195 @@
 void saatAyar() {
-  while (!digitalRead(btOk)) {
-    myRTC.updateTime();
-    u8g.drawFrame(2, 1, 126, 63);
-    u8g.setPrintPos(10, 20);
-    if (myRTC.hours >= 0 && myRTC.hours <= 9) {
-      u8g.print("0");
-    }
-    u8g.print(myRTC.hours);
-    u8g.print(":");
-    if (myRTC.minutes >= 0 && myRTC.minutes <= 9) {
-      u8g.print("0");
-    }
-    u8g.print(myRTC.minutes);
-    if (digitalRead(btSet)) {
-      myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, ((myRTC.hours + 1) % 24), myRTC.dayofweek, myRTC.dayofmonth, myRTC.month, myRTC.year);
-    }
-    u8g.firstPage();
-    do {
-      saatAyar();
-    } while (u8g.nextPage());
+  myRTC.updateTime();
+  u8g.drawFrame(2, 1, 126, 63);
+  u8g.setPrintPos(10, 40);
+  if (myRTC.hours >= 0 && myRTC.hours <= 9) {
+    u8g.print("0");
   }
-  u8g.firstPage();
-  do {
-    dakikaAyar();
-  } while (u8g.nextPage());
+  u8g.print(myRTC.hours);
+  u8g.print(":  ");
+  if (digitalRead(btSet)) {
+    myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, ((myRTC.hours + 1) % 24), myRTC.dayofweek, myRTC.dayofmonth, myRTC.month, myRTC.year);
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+  } else if (digitalRead(btOk)) {
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+    is_changed = 1;
+    ayar();
+  }
 }
 
+
 void dakikaAyar() {
-  while (!digitalRead(btOk)) {
-    myRTC.updateTime();
-    u8g.drawFrame(2, 1, 126, 63);
-    u8g.setPrintPos(10, 20);
-    if (myRTC.hours >= 0 && myRTC.hours <= 9) {
-      u8g.print("0");
-    }
-    u8g.print(myRTC.hours);
-    u8g.print(":");
-    if (myRTC.minutes >= 0 && myRTC.minutes <= 9) {
-      u8g.print("0");
-    }
-    u8g.print(myRTC.minutes);
-    if (digitalRead(btSet)) {
-      myRTC.setDS1302Time(myRTC.seconds, ((myRTC.minutes + 1) % 60), myRTC.hours, myRTC.dayofweek, myRTC.dayofmonth, myRTC.month, myRTC.year);
-    }
-    u8g.firstPage();
-    do {
-      dakikaAyar();
-    } while (u8g.nextPage());
+  myRTC.updateTime();
+  u8g.drawFrame(2, 1, 126, 63);
+  u8g.setPrintPos(10, 40);
+  u8g.print("  :");
+  if (myRTC.minutes >= 0 && myRTC.minutes <= 9) {
+    u8g.print("0");
   }
-  u8g.firstPage();
-  do {
-    gunAyar();
-  } while (u8g.nextPage());
+  u8g.print(myRTC.minutes);
+  if (digitalRead(btSet)) {
+    myRTC.setDS1302Time(myRTC.seconds, ((myRTC.minutes + 1) % 60), myRTC.hours, myRTC.dayofweek, myRTC.dayofmonth, myRTC.month, myRTC.year);
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+  } else if (digitalRead(btOk)) {
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+    is_changed = 2;
+    ayar();
+  }
 }
 
 void gunAyar() {
-  while (!digitalRead(btOk)) {
-    myRTC.updateTime();
-    u8g.drawFrame(2, 1, 126, 63);
-    u8g.setPrintPos(10, 20);
-    if (myRTC.dayofweek == 1) {
-      u8g.print("Pazartesi");
-    } else if (myRTC.dayofweek == 2) {
-      u8g.print("Sali");
-    } else if (myRTC.dayofweek == 3) {
-      u8g.print("Carsamba");
-    } else if (myRTC.dayofweek == 4) {
-      u8g.print("Persembe");
-    } else if (myRTC.dayofweek == 5) {
-      u8g.print("Cuma");
-    } else if (myRTC.dayofweek == 6) {
-      u8g.print("Cumartesi");
-    } else {
-      u8g.print("Pazar");
-    }
-    if (digitalRead(btSet)) {
-      myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, myRTC.hours, ((myRTC.dayofweek + 1) % 7), myRTC.dayofmonth, myRTC.month, myRTC.year);
-    }
-    u8g.firstPage();
-    do {
-      gunAyar();
-    } while (u8g.nextPage());
+  myRTC.updateTime();
+  u8g.drawFrame(2, 1, 126, 63);
+  u8g.setPrintPos(10, 40);
+  if (myRTC.dayofweek == 1) {
+    u8g.print("Pazartesi");
+  } else if (myRTC.dayofweek == 2) {
+    u8g.print("Sali");
+  } else if (myRTC.dayofweek == 3) {
+    u8g.print("Carsamba");
+  } else if (myRTC.dayofweek == 4) {
+    u8g.print("Persembe");
+  } else if (myRTC.dayofweek == 5) {
+    u8g.print("Cuma");
+  } else if (myRTC.dayofweek == 6) {
+    u8g.print("Cumartesi");
+  } else {
+    u8g.print("Pazar");
   }
-  u8g.firstPage();
-  do {
-    ay_gunuAyar();
-  } while (u8g.nextPage());
+  if (digitalRead(btSet)) {
+    myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, myRTC.hours, ((myRTC.dayofweek + 1) % 7), myRTC.dayofmonth, myRTC.month, myRTC.year);
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+  } else if (digitalRead(btOk)) {
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+    is_changed = 3;
+    ayar();
+  }
 }
 
 void ay_gunuAyar() {
   int day;
-  while (!digitalRead(btOk)) {
-    myRTC.updateTime();
-    u8g.drawFrame(2, 1, 126, 63);
-    u8g.setPrintPos(10, 20);
-    if (myRTC.dayofmonth > 0 && myRTC.dayofmonth <= 9) {
-      u8g.print("0");
-    }
-    u8g.print(myRTC.dayofmonth);
-    u8g.print("/");
-    if (myRTC.month > 0 && myRTC.month <= 9) {
-      u8g.print("0");
-    }
-
-    //Month printer
-
-    u8g.print(myRTC.month);
-    u8g.print("/");
-    u8g.print(myRTC.year);
-
-    if (digitalRead(btSet)) {
-      day = (myRTC.dayofmonth + 1) % 31;
-      if (day == 0){
-        day = 31;
-      }
-      myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, myRTC.hours, myRTC.dayofweek, day, myRTC.month, myRTC.year);
-    }
-    u8g.firstPage();
-    do {
-      ay_gunuAyar();
-    } while (u8g.nextPage());
+  myRTC.updateTime();
+  u8g.drawFrame(2, 1, 126, 63);
+  u8g.setPrintPos(10, 40);
+  if (myRTC.dayofmonth > 0 && myRTC.dayofmonth <= 9) {
+    u8g.print("0");
   }
-  u8g.firstPage();
-  do {
-    ayAyar();
-  } while (u8g.nextPage());
+  u8g.print(myRTC.dayofmonth);
+  u8g.print("/");
+
+  //Month printer
+
+  u8g.print("  /    ");
+
+  if (digitalRead(btSet)) {
+    day = (myRTC.dayofmonth + 1) % 31;
+    if (day == 0) {
+      day = 31;
+    }
+    myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, myRTC.hours, myRTC.dayofweek, day, myRTC.month, myRTC.year);
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+  } else if (digitalRead(btOk)) {
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+    is_changed = 4;
+    ayar();
+  }
 }
 
 void ayAyar() {
   int ay;
-  while (!digitalRead(btOk)) {
-    myRTC.updateTime();
-    u8g.drawFrame(2, 1, 126, 63);
-    u8g.setPrintPos(10, 20);
-    if (myRTC.dayofmonth > 0 && myRTC.dayofmonth <= 9) {
-      u8g.print("0");
-    }
-    u8g.print(myRTC.dayofmonth);
-    u8g.print("/");
-    if (myRTC.month > 0 && myRTC.month <= 9) {
-      u8g.print("0");
-    }
-
-    //Month printer
-
-    u8g.print(myRTC.month);
-    u8g.print("/");
-    u8g.print(myRTC.year);
-
-    if (digitalRead(btSet)) {
-      ay = (myRTC.month + 1) % 12;
-      if(ay == 0){
-        ay = 12;
-      }
-      myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, myRTC.hours, myRTC.dayofweek, myRTC.dayofmonth, ay, myRTC.year);
-    }
-    u8g.firstPage();
-    do {
-      ayAyar();
-    } while (u8g.nextPage());
+  myRTC.updateTime();
+  u8g.drawFrame(2, 1, 126, 63);
+  u8g.setPrintPos(10, 40);
+  u8g.print("  /");
+  if (myRTC.month > 0 && myRTC.month <= 9) {
+    u8g.print("0");
   }
-  u8g.firstPage();
-  do {
-    yilAyar();
-  } while (u8g.nextPage());
+
+  //Month printer
+
+  u8g.print(myRTC.month);
+  u8g.print("/    ");
+
+  if (digitalRead(btSet)) {
+    ay = (myRTC.month + 1) % 12;
+    if (ay == 0) {
+      ay = 12;
+    }
+    myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, myRTC.hours, myRTC.dayofweek, myRTC.dayofmonth, ay, myRTC.year);
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+  } else if (digitalRead(btOk)) {
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+    is_changed = 5;
+    ayar();
+  }
 }
 
-void yilAyar(){
-  while (!digitalRead(btOk)) {
-    myRTC.updateTime();
-    u8g.drawFrame(2, 1, 126, 63);
-    u8g.setPrintPos(10, 20);
-    if (myRTC.dayofmonth > 0 && myRTC.dayofmonth <= 9) {
-      u8g.print("0");
-    }
-    u8g.print(myRTC.dayofmonth);
-    u8g.print("/");
-    if (myRTC.month > 0 && myRTC.month <= 9) {
-      u8g.print("0");
-    }
+void yilAyar() {
+  myRTC.updateTime();
+  u8g.drawFrame(2, 1, 126, 63);
+  u8g.setPrintPos(10, 40);
 
-    //Month printer
+  u8g.print("  /  ");
 
-    u8g.print(myRTC.month);
-    u8g.print("/");
-    u8g.print(myRTC.year);
+  //Month printer
 
-    if (digitalRead(btSet)) {
-      myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, myRTC.hours, myRTC.dayofweek, myRTC.dayofmonth, myRTC.month, (myRTC.year + 1));
+  u8g.print(myRTC.year);
+
+  if (digitalRead(btSet)) {
+    myRTC.setDS1302Time(myRTC.seconds, myRTC.minutes, myRTC.hours, myRTC.dayofweek, myRTC.dayofmonth, myRTC.month, (myRTC.year + 1));
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
     }
-    u8g.firstPage();
-    do {
-      yilAyar();
-    } while (u8g.nextPage());
+  } else if (digitalRead(btOk)) {
+    lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
+    is_changed = 6;
+    setting = false;
+    ayar();
   }
-  loop();
 }
--- Tarih-saat-sicaklik/Tarih-saat-sicaklik.ino
+++ Tarih-saat-sicaklik/Tarih-saat-sicaklik.ino
@@ -15,17 +15,21 @@ dht11 DHT;
 
 int maxAddr = 288;  //maxAddr Calculation: 24 hours x 60 minutes / recording frequency (In this example my frequency is 5 minutes)
 int p, addr = 0;
-int sec = 50, mnt = 28, clk = 15, dayofweek = 6, dd = 24, mm = 6, yyyy = 2023;  //Date and time variables
-int nowMillis = 0, lastMillis = 0,nowMillisAyar = 0, lastMillisAyar = 0;        //time control variables
+int sec = 00, mnt = 28, clk = 15, dayofweek = 6, dd = 24, mm = 6, yyyy = 2023;  //Date and time variables
+int nowMillis = 0, lastMillis = 0, nowMillisAyar = 0, lastMillisAyar = 0;       //time control variables
 int pageCount = 2;                                                              //number of pages variable
 float ort_sck;
+
+int is_changed = 0;  //0: hours, 1: minutes, 2: dayofweek, 3: dayofmonth, 4: month, 5: year
 
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
 
 //Page's settings
 
 void setup() {
-  //myRTC.setDS1302Time(sec, mnt, clk, dayofweek, dd, mm, yyyy); //Just set the time by running it on the first installation, then comment line and upload again
+  //myRTC.setDS1302Time(sec, mnt, clk, dayofweek, dd, mm, yyyy);  //Just set the time by running it on the first installation, then comment line and upload again
+  pinMode(btSet, INPUT);
+  pinMode(btOk, INPUT);
   myRTC.updateTime();
   Serial.begin(9600);
   int chk = DHT.read(DHT11PIN);
@@ -38,17 +42,64 @@ void setup() {
   nowMillis = millis();
   while ((lastMillis - nowMillis) < 3000) {
     u8g.firstPage();
-      do {
-        sayfaMarka();
-      } while (u8g.nextPage());
+    do {
+      sayfaMarka();
+    } while (u8g.nextPage());
     lastMillis = millis();
   }
 }
 
 bool writed = false;
+bool setting = false;
+
+void ayar() {
+  setting = true;
+  switch (is_changed) {
+    case (0):
+      u8g.firstPage();
+      do {
+        saatAyar();
+      } while (u8g.nextPage());
+
+      break;
+    case (1):
+      u8g.firstPage();
+      do {
+        dakikaAyar();
+      } while (u8g.nextPage());
+      break;
+    case (2):
+      u8g.firstPage();
+      do {
+        gunAyar();
+      } while (u8g.nextPage());
+      break;
+    case (3):
+      u8g.firstPage();
+      do {
+        ay_gunuAyar();
+      } while (u8g.nextPage());
+      break;
+    case (4):
+      u8g.firstPage();
+      do {
+        ayAyar();
+      } while (u8g.nextPage());
+      break;
+    case (5):
+      u8g.firstPage();
+      do {
+        yilAyar();
+      } while (u8g.nextPage());
+      break;
+      case(6):
+      is_changed = 0;
+      setting = false;
+      break;
+  }
+}
 
 void pageChange() {
-
   lastMillis = millis();
   switch (p) {
     case (0):
@@ -64,16 +115,6 @@ void pageChange() {
       } while (u8g.nextPage());
       break;
   }
-}
-void ayar(){
-  nowMillisAyar = millis();
-  if((nowMillisAyar - lastMillisAyar >= 3000) && (!digitalRead(btSet) || !digitalRead(btOk))){
-    loop();
-  }
-  u8g.firstPage();
-      do {
-        saatAyar();
-      } while (u8g.nextPage());
 }
 
 void loop() {
@@ -91,12 +132,16 @@ void loop() {
     writed = false;
   }
   nowMillis = millis();
-  if (nowMillis - lastMillis >= 2500) {
+  if ((nowMillis - lastMillis >= 2500) && !setting) {
     p = (p + 1) % pageCount;
     pageChange();
   }
-  if(digitalRead(btSet)){
+  if (digitalRead(btSet)|| digitalRead(btOk)) {
     lastMillisAyar = millis();
+    nowMillisAyar = millis();
+    while ((nowMillisAyar - lastMillisAyar) < 200) {
+      nowMillisAyar = millis();
+    }
     ayar();
   }
 }
